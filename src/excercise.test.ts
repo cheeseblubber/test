577b8f592788bc1bf6f38d3172a37ee615480c70
Kam Leung <kamleung911@gmail.com>
2025-01-11 19:18:12 -0800

    excercise template

--- src/excercise.test.ts
+++ src/excercise.test.ts
@@ -1,4 +1,4 @@
-import { add } from "../src/calc";
+import { add } from "./excercise";
 describe("test add function", () => {
   it("should return 15 for add(10,5)", () => {
     expect(add(10, 5)).toBe(15);
